3aa3403eff22801469e5a4b2c8ba2dea47ee8e3e
Elliott Miller <57118301+elliottbmiller@users.noreply.github.com>
2026-01-09 14:27:06 -0500

    header

---
+++ my-app/src/lib/components/navigationLink.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    let { title, linkText } = $props();
+</script>
+<a href="/{linkText}" style="text-decoration: none">
+    <div class="mb-2 py-1.5 pl-4 pr-2 h-9 text-black min-w-fit w-full flex justify-between text-nowrap rounded-md text-left hover:bg-[#dfdfdf] group" >
+        {title}
+    </div>
+</a>
