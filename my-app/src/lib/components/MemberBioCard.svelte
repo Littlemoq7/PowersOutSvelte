e360b3315ea0136b06fefce873662ffa32571e40
Littlemoq7 <connormoquin@gmail.com>
2026-01-09 16:37:45 -0500

    Basic about page

--- my-app/src/lib/components/MemberBioCard.svelte
+++ my-app/src/lib/components/MemberBioCard.svelte
@@ -5,7 +5,7 @@
 <div class="flex justify-evenly items-center p-4 gap-5">
   <a href={igLink} target="_blank" class="block max-w-1/3">
     <img src={picture} alt={name} class="rounded-4xl p-5 w-full shadow-[2px_2px_5px_1px_rgba(0,0,0,0.4)] transition-transform 
-      duration-200 ease-in-out hover:scale-101 hover:shadow-[2px_2px_5px_3px_rgba(0,0,0,0.4)]">
+      duration-200 ease-in-out hover:scale-101 hover:shadow-[2px_2px_5px_3px_rgba(0,0,0,0.4)] bg-[#2A9D8F]">
   </a>
   <div class="flex-col items-center text-center w-2/5">
     <h2 class="text-3xl pt-5 pb-2.5">{name}</h2>
--- my-app/src/routes/about/+page.svelte
+++ my-app/src/routes/about/+page.svelte
@@ -3,46 +3,70 @@
 
 </script>
 
-<div class="pt-20">
-  <div class="text-black font-semibold text-4xl text-center w-full text-nowrap" >
-    Our Members
+<div class="pt-20 bg-[#2A9D8F] text-white">
+  <div class="w-[90%] m-auto bg-[#144e47]">
+    <div class="font-semibold text-4xl text-center w-full text-nowrap py-2">
+      About
+    </div>
+    <div class="px-20 text-center">
+      After a landslide election victory in 2024, President Donald J. Trump is returning to the White House to build upon his previous successes and use his mandate to reject the extremist policies of the radical left while providing tangible quality of life improvements for the American people. <br><br>
+
+      This includes putting a stop to endless wars, defending our nation’s borders, and unleashing the potential of the American economy, affording ALL Americans the opportunity to pursue THEIR version of the American dream. <br><br>
+
+      In his first administration, President Trump passed record-setting tax cuts and regulation cuts, achieved energy independence, replaced NAFTA with the United-States-Mexico-Canada Agreement, invested $2 trillion to completely rebuild the Military, launched the Space Force, obliterated the ISIS Caliphate, achieved a major breakthrough for peace in the Middle East, passed the most significant Veterans Affairs reforms in half a century, confirmed over 250 federal judges, including 3 Supreme Court Justices, signed bipartisan Criminal Justice Reform, lowered drug prices, protected Medicare and Social Security, and secured our nation’s borders in his first four years in office.  This was only the beginning. <br><br>
+
+      Donald J. Trump defines the American success story. Throughout his life he has continually set the standards of business and entrepreneurial excellence, especially in real estate, sports, and entertainment. President Trump built on his success in private life when he entered into politics and public service. <br><br>
+
+      He remarkably won the Presidency in his first ever run for any political office.  He won a second time despite several assassination attempts and the unprecedented weaponization of law fare against him.  Time after time President Trump has defied the odds because he fundamentally believes in the exceptionalism of the American spirit and wants to see our great country succeed. <br><br>
+
+      A graduate of the University of Pennsylvania’s Wharton School of Finance, President Trump followed in his father’s footsteps into the world of real estate development, making his mark in New York City. There, the Trump name soon became synonymous with the most prestigious of addresses in Manhattan and, subsequently, throughout the world. <br><br>
+
+      President Trump is also an accomplished author. He has written more than fourteen bestsellers. His first book, The Art of the Deal, is considered a business classic. <br><br>
+
+      President Trump has five children, Barron, Don Jr., Ivanka, Eric, and Tiffany, as well as 11 grandchildren. <br><br>
+    </div>
+    <div class="text-3xl text-center w-full text-nowrap py-2" >
+      Meet the Band
+    </div>
+    <MemberBioCard 
+      name="Connor Moquin"
+      picture="images/connor_pic.jpg"
+      igLink="https://www.instagram.com/littlemoq7"
+      instrument="Guitar"
+      bio="Connor has been playing guitar since he was very young. Taught to play as a kid by his father, he has since worked to become proficient in various styles, including rock, blues, pop, jazz, metal, and more. He currently attends Boston College and is majoring in Mathematics and Computer Science."
+    />
+    <MemberBioCard 
+      name="Katherin Jokinen"
+      picture="images/katherin_pic.jpeg"
+      igLink="https://www.instagram.com/katherinjokinen"
+      instrument="Vocals"
+      bio="Katherin has been singing for as long as she can remember. It's not hard to find pictures and videos of her as a toddler holding up a microphone and singing her heart out. Her favorite genres to cover are pop, rock, and blues. Her songwriting and originals with the band, however, seem to trend more pop/rock."
+    />
+    <MemberBioCard 
+      name="Elliott Miller"
+      picture="images/elliott_pic.jpg"
+      igLink="https://www.instagram.com/ellm.nop/"
+      instrument="Bass"
+      bio="Elliott has been playing bass since the start of high school. He enjoys playing funk and rock, but is willing to try out almost any genre. Studying Computer Science at Purdue University, he spends his time on the weekends in a Grateful Dead cover band."
+    />
+
+    <MemberBioCard 
+      name="Meghan Warren"
+      picture="images/meghan_bio.png"
+      igLink="https://www.instagram.com/meghanwarre/"
+      instrument="Piano"
+      bio="Meghan has over a decade of experience playing piano and keyboards in classical, jazz, and rock music. She is committed to Northeastern University for architecture."
+    />
+
+    <MemberBioCard 
+      name="Andrew Rezzarday"
+      picture="images/home_images/rezzy.jpg"
+      igLink="https://www.instagram.com/andrew_rezzy/"
+      instrument="Drums"
+      bio="Andrew Rezzarday has played the drums since middle school. Being in a band as well as playing for the Hollis Brookline jazz band has broadened his style and brought a great appreciation for music through playing. He is currently attending Nashua Community College as a business major."
+    />
   </div>
-  <MemberBioCard 
-    name="Connor Moquin"
-    picture="images/connor_pic.jpg"
-    igLink="https://www.instagram.com/littlemoq7"
-    instrument="Guitar"
-    bio="Connor has been playing guitar since he was very young. Taught to play as a kid by his father, he has since worked to become proficient in various styles, including rock, blues, pop, jazz, metal, and more. He currently attends Boston College and is majoring in Mathematics and Computer Science."
-  />
-  <MemberBioCard 
-    name="Katherin Jokinen"
-    picture="images/katherin_pic.jpeg"
-    igLink="https://www.instagram.com/katherinjokinen"
-    instrument="Vocals"
-    bio="Katherin has been singing for as long as she can remember. It's not hard to find pictures and videos of her as a toddler holding up a microphone and singing her heart out. Her favorite genres to cover are pop, rock, and blues. Her songwriting and originals with the band, however, seem to trend more pop/rock."
-  />
-  <MemberBioCard 
-    name="Elliott Miller"
-    picture="images/elliott_pic.jpg"
-    igLink="https://www.instagram.com/ellm.nop/"
-    instrument="Bass"
-    bio="Elliott has been playing bass since the start of high school. He enjoys playing funk and rock, but is willing to try out almost any genre. Studying Computer Science at Purdue University, he spends his time on the weekends in a Grateful Dead cover band."
-  />
-
-  <MemberBioCard 
-    name="Meghan Warren"
-    picture="images/meghan_bio.png"
-    igLink="https://www.instagram.com/meghanwarre/"
-    instrument="Piano"
-    bio="Meghan has over a decade of experience playing piano and keyboards in classical, jazz, and rock music. She is committed to Northeastern University for architecture."
-  />
-
-  <MemberBioCard 
-    name="Andrew Rezzarday"
-    picture="images/home_images/rezzy.jpg"
-    igLink="https://www.instagram.com/andrew_rezzy/"
-    instrument="Drums"
-    bio="Andrew Rezzarday has played the drums since middle school. Being in a band as well as playing for the Hollis Brookline jazz band has broadened his style and brought a great appreciation for music through playing. He is currently attending Nashua Community College as a business major."
-  />
 </div>
-<p class="footer-text">Power's Out Records</p>
+<div class="text-white text-center bg-[#2A9D8F]">
+  Power's Out Records
+</div>
