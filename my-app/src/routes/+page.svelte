521361e74d6df821c62b978324ea619c0f4ad523
Elliott Miller <57118301+elliottbmiller@users.noreply.github.com>
2026-01-09 16:07:41 -0500

    Update +page.svelte

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <div class="w-full h-[60vh] overflow-hidden relative">
     <div class="bg-black w-full h-full opacity-30 absolute">
     </div>
-    <video autoplay loop muted playsinline class="w-full h-full object-cover">
+    <video autoplay loop muted playsinline class="w-full h-full object-cover object-center-200">
       <source src="images/short_band_clips.mp4" type="video/mp4">
     </video>
 </div>
