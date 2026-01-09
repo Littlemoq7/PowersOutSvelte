4a7893e6df82eedf04df55a5851c2c2aaf01a905
Elliott Miller <57118301+elliottbmiller@users.noreply.github.com>
2026-01-09 16:34:43 -0500

    Update +page.svelte

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,4 +1,26 @@
-<div class="w-full h-[60vh] overflow-hidden relative">
+<div class="w-full h-[65vh] overflow-hidden relative">
+    <div class="w-full flex-row absolute top-60 flex justify-between">
+        <div class="flex">
+        </div>
+        <div class="flex">
+            <a class="text-3xl font-semibold text-white bg-cyan-700 pt-5 pb-5 pr-9 pl-9 rounded-[50px] hover:opacity-70 transition-opacity z-1" href="/discography"aria-label="Music">
+                Music
+            </a>
+
+        </div>
+        <div class="flex">
+            <a class="text-3xl font-semibold text-white bg-cyan-700 pt-5 pb-5 pr-9 pl-9 rounded-[50px] hover:opacity-70 transition-opacity z-1" href="/about"aria-label="Members">
+                Members
+            </a>
+        </div>
+        <div class="flex">
+            <a class="text-3xl font-semibold text-white bg-cyan-700 pt-5 pb-5 pr-9 pl-9 rounded-[50px] hover:opacity-70 transition-opacity z-1" href="/gallery"aria-label="Photos">
+                Photos
+            </a>
+        </div>
+        <div class="flex">
+        </div>
+    </div>
     <div class="bg-black w-full h-full opacity-30 absolute">
     </div>
     <video autoplay loop muted playsinline class="w-full h-full object-cover">
