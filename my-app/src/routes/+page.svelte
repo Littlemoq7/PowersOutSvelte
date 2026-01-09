f471333f8b79dc85a260c5cb2dc16b7854d7b5c3
Littlemoq7 <connormoquin@gmail.com>
2026-01-09 16:07:38 -0500

    Poster link

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <div class="w-full h-[60vh] overflow-hidden relative">
     <div class="bg-black w-full h-full opacity-30 absolute">
     </div>
-    <video autoplay loop muted playsinline class="w-full h-full object-cover">
+    <video autoplay loop muted playsinline poster="images/header_poster.jpg" class="w-full h-full object-cover">
       <source src="images/short_band_clips.mp4" type="video/mp4">
     </video>
 </div>
