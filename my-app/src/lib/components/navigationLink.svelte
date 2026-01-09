c5dd31bc63ffe6bb0759b8293856a94ce9b80655
Elliott Miller <57118301+elliottbmiller@users.noreply.github.com>
2026-01-09 15:03:29 -0500

    header

--- my-app/src/lib/components/navigationLink.svelte
+++ my-app/src/lib/components/navigationLink.svelte
@@ -1,8 +1,14 @@
 <script lang="ts">
+    import { page } from '$app/state';
     let { title, linkText } = $props();
 </script>
 <a href="/{linkText}" style="text-decoration: none">
-    <div class="mb-2 py-1.5 pl-4 pr-2 h-9 text-black min-w-fit w-full flex justify-between text-nowrap rounded-md text-left hover:bg-[#dfdfdf] group" >
+    <div class="p-3 h-9 pb-1 text-white min-w-fit w-full flex justify-between text-nowrap text-sm rounded-md text-left hover:font-bold">
         {title}
     </div>
+    {#if page.url.pathname == "/" + linkText}
+    <div class="h-px bg-white flex mx-3">
+
+    </div>
+    {/if}
 </a>
--- my-app/src/routes/+layout.svelte
+++ my-app/src/routes/+layout.svelte
@@ -2,16 +2,45 @@
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
 	import NavigationLink from '$lib/components/navigationLink.svelte';
+  import NavigationBarIcon from '$lib/components/navigationBarIcon.svelte';
 
 	let { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
-<header>
-	<NavigationLink title="Home" linkText=""></NavigationLink>
-	<NavigationLink title="About" linkText="about"></NavigationLink>
-	<NavigationLink title="Gallery" linkText="gallery"></NavigationLink>
-	<NavigationLink title="Discography" linkText="discography"></NavigationLink>
-	<NavigationLink title="Contact" linkText="contact"></NavigationLink>
+<header class="row bg-amber-700 h-20 justify-between w-full">
+	<div class="flex flex-row left">
+		<NavigationLink title="Home" linkText=""></NavigationLink>
+		<NavigationLink title="About" linkText="about"></NavigationLink>
+		<NavigationLink title="Gallery" linkText="gallery"></NavigationLink>
+		<NavigationLink title="Discography" linkText="discography"></NavigationLink>
+		<NavigationLink title="Contact" linkText="contact"></NavigationLink>
+	</div>
+	<div class="text-white font-semibold text-4xl center text-nowrap" >
+		Power's Out
+	</div>
+	<div class="flex flex-row right">
+		<NavigationBarIcon></NavigationBarIcon>
+	</div>
 </header>
 {@render children()}
+
+<style>
+	.row {
+		display: grid;
+		grid-template-columns: 1fr auto 1fr;
+		align-items: center;
+	}
+
+	.center {
+		justify-self: center;
+	}
+
+	.left {
+		justify-self: end;
+	}
+
+	.right {
+		justify-self: start;
+	}
+</style>
