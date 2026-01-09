04709b36484bcd471b3d457380dd3652760a1e97
Littlemoq7 <connormoquin@gmail.com>
2026-01-09 15:01:08 -0500

    Made bios look like the old site

---
+++ my-app/src/lib/components/MemberBioCard.svelte
@@ -0,0 +1,15 @@
+<script>
+  let { name, picture, igLink, instrument, bio } = $props();
+</script>
+
+<div class="flex justify-evenly items-center p-4 gap-5">
+  <a href={igLink} target="_blank" class="block max-w-1/3">
+    <img src={picture} alt={name} class="rounded-4xl p-5 w-full shadow-[2px_2px_5px_1px_rgba(0,0,0,0.4)] transition-transform 
+      duration-200 ease-in-out hover:scale-101 hover:shadow-[2px_2px_5px_3px_rgba(0,0,0,0.4)]">
+  </a>
+  <div class="flex-col items-center text-center w-2/5">
+    <h2 class="text-3xl pt-5 pb-2.5">{name}</h2>
+    <h3 class="text-xl pt-0 pb-2.5">{instrument}</h3>
+    <p>{bio}</p>
+  </div>
+</div>
