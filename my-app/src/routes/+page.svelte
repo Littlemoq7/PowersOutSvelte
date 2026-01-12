41c0981c405cff1d4b88c023a41d704769c96b6f
Littlemoq7 <connormoquin@gmail.com>
2026-01-12 17:49:34 -0500

    Front page changes

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -18,40 +18,50 @@
       <source src="images/short_band_clips.mp4" type="video/mp4">
     </video>
 </div>
-<div class="flex-col h-300 bg-[#a8d4cf] text-white pt-4"> 
-  <div class="text-center bg-[#259185] w-2/3 m-auto rounded-4xl p-2">
-    <h2 class="font-semibold text-xl">Hollis, NH band founded in 2021.</h2>
-    Plays originals and covers across genres like rock, pop, blues, country, funk, jam band, and more.
-    Click the YouTube icon to watch some of our performances, and see the contact page for booking and inquiries.
-    <div class="links">
-      <a href="https://open.spotify.com/artist/4qEGLVNJT3OfCqBKsRHvcG?si=GoAw_CgARNCr2Pi8hDrmdQ" target="_blank"><img src="images/spotify-logo.png" alt="Spotify Logo"></a>
-      <a href="https://music.apple.com/us/artist/powers-out/1637085670" target="_blank"><img src="images/apple-music-icon.png" class="rounded-xl" alt="Apple Music Logo"></a>
-      <a href="https://www.youtube.com/@powersoutband" target="_blank"><img src="images/youtube-logo.png" alt="Youtube Logo"></a>
-      <a href="https://www.instagram.com/powersoutband/" target="_blank"><img src="images/instagram_icon.png" alt="Instagram Icon"></a>
+<div class="flex-col h-200 bg-[#a8d4cf] text-white pt-4"> 
+  <div class="flex-col w-4/5 mx-auto bg-[#259185] rounded">
+    <div class="w-full px-10 py-5 text-center">
+      <h2 class="font-semibold text-xl">Hollis, NH band founded in 2021.</h2>
+      Plays originals and covers across genres like rock, pop, blues, country, funk, jam band, and more.
+      Click the YouTube icon to watch some of our performances, and see the contact page for booking and inquiries.
+      <div class="links">
+        <a href="https://open.spotify.com/artist/4qEGLVNJT3OfCqBKsRHvcG?si=GoAw_CgARNCr2Pi8hDrmdQ" target="_blank"><img src="images/spotify-logo.png" alt="Spotify Logo"></a>
+        <a href="https://music.apple.com/us/artist/powers-out/1637085670" target="_blank"><img src="images/apple-music-icon.png" class="rounded-xl" alt="Apple Music Logo"></a>
+        <a href="https://www.youtube.com/@powersoutband" target="_blank"><img src="images/youtube-logo.png" alt="Youtube Logo"></a>
+        <a href="https://www.instagram.com/powersoutband/" target="_blank"><img src="images/instagram_icon.png" alt="Instagram Icon"></a>
+      </div>
     </div>
-  </div>
-  <div class="bg-[#259185] rounded-4xl my-4 p-4 w-[90%] mx-auto">
-    <div class="m-auto text-center pb-2">
-      <h2 class="font-semibold text-xl">Live Performances</h2>
-    </div>
-    <div class="flex justify-evenly">
-      <iframe
-        class="w-2/5"
-        src="https://www.youtube.com/watch?v=0dMOQbHaTFQ"
-        title="YouTube video 1"
-        frameborder="0"
-        allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen>
-      </iframe>
+    <!-- </div> -->
+    <!-- <div class="bg-[#259185] rounded-4xl my-4 p-4 w-[90%] mx-auto"> -->
+    <div class="w-full px-10 py-5">
+      <div class="m-auto text-center pb-2">
+        <h2 class="font-semibold text-xl">Live Performances</h2>
+      </div>
+      <div class="flex justify-evenly">
+        <iframe
+          class="w-2/5 aspect-video"
+          src="https://www.youtube.com/embed/0dMOQbHaTFQ"
+          title="YouTube video 1"
+          frameborder="0"
+          allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen>
+        </iframe>
 
-      <iframe
-        class="w-2/5"
-        src="https://www.youtube.com/watch?v=7ck34b_bklE"
-        title="YouTube video 2"
-        frameborder="0"
-        allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen>
-      </iframe>
+        <iframe
+          class="w-2/5 aspect-video"
+          src="https://www.youtube.com/embed/7ck34b_bklE"
+          title="YouTube video 2"
+          frameborder="0"
+          allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen>
+        </iframe>
+      </div>
+    </div>
+    <div class="w-full px-10 py-5">
+      <div class="m-auto text-center pb-2">
+        <h2 class="font-semibold text-xl">Studio Recordings</h2>
+      </div>
+      <iframe src="https://open.spotify.com/embed/album/5bdC7E9TVlThVvLa6mfOLf" title="Spotify Web Player" frameborder="0" allow="encrypted-media" class="w-2/3 h-100 mx-auto"></iframe>
     </div>
   </div>
 </div>
