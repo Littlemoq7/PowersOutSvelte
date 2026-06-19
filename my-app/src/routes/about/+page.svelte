02a27292b850fa8149d91fc5897682a5b3255487
Littlemoq7 <connormoquin@gmail.com>
2026-06-18 20:17:45 -0400

    Contact page and photo sizes

--- my-app/src/routes/about/+page.svelte
+++ my-app/src/routes/about/+page.svelte
@@ -17,21 +17,21 @@
     </div>
     <MemberBioCard 
       name="Connor Moquin"
-      picture="images/connor_pic.jpg"
+      picture="images/connor_pic.webp"
       igLink="https://www.instagram.com/littlemoq7"
       instrument="Guitar"
       bio="Connor has been playing guitar since he was very young. Taught to play as a kid by his father, he has since worked to become proficient in various styles, including rock, blues, pop, jazz, metal, and more. He currently attends Boston College and is majoring in Mathematics and Computer Science."
     />
     <MemberBioCard 
       name="Katherin Jokinen"
-      picture="images/katherin_pic.jpeg"
+      picture="images/katherin_pic.webp"
       igLink="https://www.instagram.com/katherinjokinen"
       instrument="Vocals"
       bio="Katherin has been singing for as long as she can remember. It's not hard to find pictures and videos of her as a toddler holding up a microphone and singing her heart out. Her favorite genres to cover are pop, rock, and blues. Her songwriting and originals with the band, however, seem to trend more pop/rock."
     />
     <MemberBioCard 
       name="Elliott Miller"
-      picture="images/elliott_pic.jpg"
+      picture="images/elliott_pic.webp"
       igLink="https://www.instagram.com/ellm.nop/"
       instrument="Bass"
       bio="Elliott has been playing bass since the start of high school. He enjoys playing funk and rock, but is willing to try out almost any genre. Studying Computer Science at Purdue University, he spends his time on the weekends in a Grateful Dead cover band."
@@ -39,7 +39,7 @@
 
     <MemberBioCard 
       name="Meghan Warren"
-      picture="images/meghan_bio.png"
+      picture="images/meghan_bio.webp"
       igLink="https://www.instagram.com/meghanwarre/"
       instrument="Piano"
       bio="Meghan has over a decade of experience playing piano and keyboards in classical, jazz, and rock music. She is committed to Northeastern University for architecture."
@@ -47,7 +47,7 @@
 
     <MemberBioCard 
       name="Andrew Rezzarday"
-      picture="images/andrew_pic.JPG"
+      picture="images/andrew_pic.webp"
       igLink="https://www.instagram.com/andrew_rezzy/"
       instrument="Drums"
       bio="Andrew Rezzarday has played the drums since middle school. Being in a band as well as playing for the Hollis Brookline jazz band has broadened his style and brought a great appreciation for music through playing. He is currently attending Nashua Community College as a business major."
--- my-app/src/routes/contact/+page.svelte
+++ my-app/src/routes/contact/+page.svelte
@@ -1,9 +1,9 @@
-<div class="bg-[#a8d4cf] min-h-screen pt-20 flex flex-col">
-  <section class="flex flex-col justify-center items-center px-5 min-h-[60vh] flex-1 box-border w-[90%]  max-w-300 mx-auto bg-[#259185]">
+<div class="bg-[#a8d4cf] min-h-[calc(100vh-1.5rem)] flex flex-col">
+  <section class="flex flex-col justify-center items-center px-5 flex-1 box-border w-[90%] max-w-300 mx-auto bg-[#259185]">
     <h1 class="text-white text-3xl font-bold font-sans w-full text-center py-6">CONTACT THE BAND</h1>
-    <div class="w-4/5 mx-auto">
+    <div class="w-full">
       <p class="text-xl text-white text-center pt-2.5 pb-3.75">Fill in your contact and message below: </p>
-      <form action="https://api.web3forms.com/submit" method="POST" class="bg-white p-6.25 sm:p-10 max-w-150 w-full rounded-[10px] shadow-[0_8px_20px_rgba(0,0,0,0.1)] flex flex-col mx-auto">
+      <form action="https://api.web3forms.com/submit" method="POST" class="bg-white p-6 sm:p-10 rounded-[10px] shadow-[0_8px_20px_rgba(0,0,0,0.1)] flex flex-col sm:mx-auto max-w-2xl">
         <div>
           <!-- Replace with your Access Key -->
           <input type="hidden" name="access_key" value="f7e26cd2-aaeb-424d-ab4b-6ff2ebc3286e">
@@ -19,7 +19,7 @@
           <!-- Custom Confirmation / Success Page -->
           <input type="hidden" name="redirect" value="https://www.powersout.me/contact_thanks.html">
           <div></div>
-          <button type="submit" class="block mx-auto p-3.5 text-base bg-[#333] text-white border-none rounded-md cursor-pointer transition-colors duration-300 hover:bg-[#555]">Submit Message</button>
+          <button type="submit" class="block mx-auto p-3.5 text-base bg-[#259185] text-white border-none rounded-md cursor-pointer transition-colors duration-300 hover:bg-[#134d46]">Submit Message</button>
         </div>
       </form>
     </div>
